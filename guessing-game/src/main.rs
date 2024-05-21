79abbf654dd3de742508ecc18ed0a5ebdc6f01d8
Aadith_Th <taadith@gmail.com>
2024-05-21 18:35:18 -0500

    have standard input working

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;    // for standard user input/output
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number!");
+
+    println!("please input your guess: ");
+
+    let mut guess = String::new();  // mutable/modifiable data
+
+    io::stdin() // returns handle to the std input for terminal
+        .read_line(&mut guess)  // gets input from the user and stores in guess
+        .expect("failed to read the line"); // checks for successful operation
+
+    println!("you guessed: {guess}");
 }
