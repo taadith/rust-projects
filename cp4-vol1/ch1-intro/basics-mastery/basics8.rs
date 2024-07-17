d18647a0b67b4ac8f31e343fc893ecf6a05e0280
Aadith_Th <taadith@gmail.com>
2024-07-17 12:14:36 -0500

    added and completed basics8

--- cp4-vol1/ch1-intro/basics-mastery/basics8.rs
+++ cp4-vol1/ch1-intro/basics-mastery/basics8.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::io;
+
+fn generate_subsets(n: usize) {
+    let set: Vec<usize> = (1..=n).collect();
+    let subset_count: usize = 1 << n;
     
+    for i in 0..subset_count {
+        print!("{{");
+        for j in 0..n {
+            if i & (1 << j) != 0 {
+                print!(" {:?}", set[j]);
+            }
+        }
+        println!(" }}");
+    }
+}
+
+fn main() {
+    let n: String = 
+    generate_subsets(3);
 }
